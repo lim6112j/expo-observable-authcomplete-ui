d811fbe4c2ce9fbeabe25c36402132cf5192ab44
lim byeong cheol <lim6112j@gmail.com>
2020-05-08 18:48:50 +0900

    debounce, distinctuntilchanged worked

--- App.js
+++ App.js
@@ -1,35 +1,59 @@
-import React, {useState, useEffect, useCallback} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, {useState, useEffect, useCallback, useMemo} from 'react';
+import { StyleSheet, Text, View, TextInput, ScrollView, Image } from 'react-native';
 import { of, interval, from } from 'rxjs';
 import { map, debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 const log =(msg) => (v) => console.log(msg, " => ",v);
 const obs$ = interval(1000);
+const useDebounce = (effect, delay, deps) => {
+  const callback = useCallback(effect, deps);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      callback();
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    }
+  }, [effect, delay])
+}
 const useObservable = (observable$) => {
   const [state, setState] = useState(0);
   useEffect(() => {
-    const subs = observable$.subscribe(setState);
+    const subs = observable$.subscribe(setState, err => console.log(err), () => console.log('completed'));
     return () => subs.unsubscribe();
   },[observable$]);
   return state;
 };
 const obsFunc = (text) => of(text).pipe(
-  distinctUntilChanged(),
   filter(query => query.trim().length > 0),
-  // switchMap(searchTxt => ajax.getJSON(`https://api.github.com/search/users?q=${searchTxt}`)),
-  // switchMap(v => [v]),
+  switchMap(searchTxt => ajax.getJSON(`https://api.github.com/search/users?q=${searchTxt}`)),
   tap(log('query'))
 );
+let obsText = '';
 export default function App() {
+
   const [text, setText] = useState('');
-  const textval = useObservable(obsFunc(text));
+  useDebounce( () => obsText = text, 500, [text]);
+  const memoized$ = useMemo(() => obsFunc(obsText), [obsText])
+  const result = useObservable(memoized$)
   const cVal = useObservable(obs$);
   const onChangeText = (txt) => {
-    // console.log('text => ', txt)
     setText(txt);
   }
+const renderItems = (items) => items.map(item => {
   return (
-    <View style={styles.container}>
+    <>
+      <Text>{item.login}</Text>
+      <Image source={{
+            uri: item.avatar_url,
+          }} 
+      />
+    </>
+    );
+});
+  useEffect(() => console.log('use Effect'), [])
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
       <Text>Autocomplete Search github user</Text>
       <Text>{cVal}</Text>
       <TextInput
@@ -38,8 +62,8 @@ export default function App() {
         // style={{height: 40}}
         placeholder="Type here to translate!">
       </TextInput>
-      <Text>{textval}</Text>
-    </View>
+      {result && result.items ? renderItems(result.items) : <Text></Text>}
+    </ScrollView>
   );
 }
 
