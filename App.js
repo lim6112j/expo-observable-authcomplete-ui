d47d0fc892ac95b176b763c6712bfd2657a371f5
lim byeong cheol <lim6112j@gmail.com>
2020-05-11 11:28:23 +0900

    text delay problem

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect, useCallback, useMemo} from 'react';
 import { StyleSheet, Text, View, TextInput, ScrollView, Image } from 'react-native';
-import { of, interval, from } from 'rxjs';
+import { of, interval, from, defer } from 'rxjs';
 import { map, debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 const log =(msg) => (v) => console.log(msg, " => ",v);
-const obs$ = interval(1000);
+// const obs$ = interval(1000);
 const useDebounce = (effect, delay, deps) => {
   const callback = useCallback(effect, deps);
   useEffect(() => {
@@ -14,7 +14,7 @@ const useDebounce = (effect, delay, deps) => {
     return () => {
       clearTimeout(handler);
     }
-  }, [effect, delay])
+  }, deps)
 }
 const useObservable = (observable$) => {
   const [state, setState] = useState(0);
@@ -26,6 +26,7 @@ const useObservable = (observable$) => {
 };
 const obsFunc = (text) => of(text).pipe(
   filter(query => query.trim().length > 0),
+  map(log('search text')),
   switchMap(searchTxt => ajax.getJSON(`https://api.github.com/search/users?q=${searchTxt}`)),
   tap(log('query'))
 );
@@ -36,8 +37,9 @@ export default function App() {
   useDebounce( () => obsText = text, 500, [text]);
   const memoized$ = useMemo(() => obsFunc(obsText), [obsText])
   const result = useObservable(memoized$)
-  const cVal = useObservable(obs$);
+  // const cVal = useObservable(obs$);
   const onChangeText = (txt) => {
+    console.log('text changed')
     setText(txt);
   }
 const renderItems = (items) => items.map(item => {
@@ -51,17 +53,17 @@ const renderItems = (items) => items.map(item => {
     </>
     );
 });
-  useEffect(() => console.log('use Effect'), [])
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text>Autocomplete Search github user</Text>
-      <Text>{cVal}</Text>
+      {/* <Text>{cVal}</Text> */}
       <TextInput
         value={text}
         onChangeText={onChangeText}
         // style={{height: 40}}
-        placeholder="Type here to translate!">
+        placeholder="Type here to search!">
       </TextInput>
+      <Text>{"\n"}</Text>
       {result && result.items ? renderItems(result.items) : <Text></Text>}
     </ScrollView>
   );
